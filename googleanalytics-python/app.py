d1b84312d9d19576a171d0d947f62b5db48991b8
analytics-piyo <32300937+AnalyticsPiyo@users.noreply.github.com>
2019-01-30 15:33:27 +0900

    Update app.py

--- googleanalytics-python/app.py
+++ googleanalytics-python/app.py
@@ -3,48 +3,21 @@
 import modules.helloanalytics as analytics
 import pprint
 
-viewid = '*********'
-
-def make_argument(type, values):
-    tmp_list = list()
-    
-    for i in values:
-        tmp_dict = dict()
-        tmp_dict[type] = i
-        tmp_list.append(tmp_dict)
-    return tmp_list
-
-def make_order(values):
-    tmp_list = list()
-    for k, v in values.items():
-        tmp_dict = dict()
-        tmp_dict["fieldName"] = k
-        tmp_dict["sortOrder"] = v
-        tmp_list.append(tmp_dict)
-    return tmp_list
+viewid = '******'
 
 def main():
-
-    orders = []
 
     start_date = '7daysAgo'
     end_date = 'today'
 
-    pre_metrics = ['ga:sessions', 'ga:pageviews']
-    metrics = make_argument("expression", pre_metrics)
-    
-    pre_dimensions = ['ga:country', 'ga:date']
-    dimensions = make_argument("name", pre_dimensions)
-
-    pre_order = {"ga:country" : "ASCENDING", "ga:date" : "ASCENDING"}
-    orders = make_order(pre_order)
     view = analytics.Analytics(viewid)
-    
-    columns = pre_dimensions + pre_metrics
-    orders = []
-        
     # 戻り値はデータフレーム型
-    df = view.get_report(start_date, end_date, metrics, dimensions, orders, columns)
+    df = view.get_report(start_date
+                        , end_date
+                        , ['sessions', 'pageviews']
+                        , ['date']
+#                        , {"ga:date" : "ASCENDING"}
+    )
     print(df)
 
 if __name__ == '__main__':
